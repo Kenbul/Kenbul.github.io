4927cb383efd305a9642f0e4b1783d18178566f6
Kenbul <arigandap@gmail.com>
2025-09-13 11:40:11 +0700

    update-layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}", "./assets/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#547792",
+            },
+        },
+    },
+    plugins: [],
+}
